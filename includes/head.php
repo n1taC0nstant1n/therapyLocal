a40ad59eb503e4b1d81e15050167801669d39ca6
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-26 18:23:34 +0200

    resolve hack

--- includes/head.php
+++ includes/head.php
@@ -8,6 +8,9 @@
 	<link rel="stylesheet" href="./style.css">
 	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
 
 	<link rel="stylesheet" href="style2.css">
     <link rel="stylesheet" href="./despre.css">
@@ -17,8 +20,7 @@
 	<link rel="stylesheet" href="responsive.css">
 	<link href="assets/fontawesome/all.min.css" rel="stylesheet">
 
-	<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
-	<!-- <script>var __adobewebfontsappname__="dreamweaver"</script> -->
+
 	<script src="assets/js/jquery.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" ></script>
 	<script src="assets/js/bootstrap/bootstrap.min.js"></script>
