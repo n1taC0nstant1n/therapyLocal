4614c192b9de9e18afcbc6539171afa2084eee90
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-26 18:27:54 +0200

    resolve hack2

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title><?php echo $title; ?></title>
+<title>Pagina</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="./style.css">
 	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap.min.css">
