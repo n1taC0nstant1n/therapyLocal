901a23a557383564f215bc176c6e00b9095d0907
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-31 16:19:39 +0300

    rezolvate fisiere negasite

--- includes/head.php
+++ includes/head.php
@@ -15,8 +15,8 @@ if($info === 'Index' || $info === '/'){
 
 <title><?php echo $info; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap--min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.-min.css" />
+	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap-min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
@@ -37,7 +37,7 @@ if($info === 'Index' || $info === '/'){
 	<link rel="stylesheet" href="./contact.css">
 	<?php } ?>
 	<link rel="stylesheet" href="responsive.css">
-	<link href="assets/fontawesome/all.-min.css" rel="stylesheet">
+	<link href="assets/fontawesome/all.min.css" rel="stylesheet">
 
 
 
@@ -45,7 +45,7 @@ if($info === 'Index' || $info === '/'){
 	<?php if($info == 'Galerie' || $info == 'Servicii'){ ?>
 	<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
 	<?php } ?>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel-min.js" ></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" ></script>
 	<script src="assets/js/bootstrap/bootstrap-min.js"></script>
 	<script src="assets/js/main.js" defer></script>
 	<?php if($info == 'Contact'){ ?>
