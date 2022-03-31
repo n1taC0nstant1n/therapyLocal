0a533cae6a5899c2ec85e32bc89d389cf9a361c3
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-31 16:12:19 +0300

    .min.css  modificate in -min.css

--- includes/head.php
+++ includes/head.php
@@ -15,8 +15,8 @@ if($info === 'Index' || $info === '/'){
 
 <title><?php echo $info; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap-min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap--min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.-min.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
@@ -37,7 +37,7 @@ if($info === 'Index' || $info === '/'){
 	<link rel="stylesheet" href="./contact.css">
 	<?php } ?>
 	<link rel="stylesheet" href="responsive.css">
-	<link href="assets/fontawesome/all.min.css" rel="stylesheet">
+	<link href="assets/fontawesome/all.-min.css" rel="stylesheet">
 
 
 
