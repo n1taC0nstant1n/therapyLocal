2cac0c69b137f2197feadad1fde6f119a2f8f3b7
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-31 14:49:06 +0300

    modified bootstrap-min.js name

--- includes/head.php
+++ includes/head.php
@@ -15,7 +15,7 @@ if($info === 'Index' || $info === '/'){
 
 <title><?php echo $info; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap-min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
